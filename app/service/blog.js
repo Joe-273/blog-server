225c6012e7c38d3147120ab3518e1d6ac421bb09
xiejie <745007854@qq.com>
2021-08-05 16:52:46 +0800

    更新服务器

--- app/service/blog.js
+++ app/service/blog.js
@@ -23,6 +23,7 @@ class BlogService extends Service {
   }
 
   async add(info) {
+    
     var result = toc(info.markdownContent).json;
 
     // 计算 toc，换成成下面的格式
@@ -171,7 +172,7 @@ class BlogService extends Service {
       }
     }
 
-    console.log(info,'info:::')
+    console.log(info,'info:::');
 
     this.validate(
       {
@@ -206,6 +207,155 @@ class BlogService extends Service {
   }
 
   async update(id, info) {
+    var result = toc(info.markdownContent).json;
+
+    // 计算 toc，换成成下面的格式
+    //   "toc": [ 
+    //     { "name": "章节1", "anchor": "title-1" },
+    //     { "name": "章节2", "anchor": "title-2", 
+    //         "children": [
+    //             { "name": "章节2-1", "anchor": "title-2-1" },
+    //             { "name": "章节2-2", "anchor": "title-2-2" },
+    //         ]
+    //     }
+    // ],
+
+    // 计算出最小标题是多少
+    // var min = 6;
+    // for (var i of result) {
+    //   if (i.lvl < min) {
+    //     min = i.lvl;
+    //   }
+    // }
+
+    // var tocResult = [];
+    // var childArr = {
+    //   h1: [],
+    //   h2: [],
+    //   h3: [],
+    //   h4: [],
+    //   h5: [],
+    //   h6: []
+    // }
+
+    // var curLvl = min; // 默认从最大的一级开始记录
+
+    // for (var item of result) {
+    //   if (item.lvl === min) {
+    //     childArr['h' + min].push({ name: item.content, anchor: item.slug });
+    //   } else {
+    //     if (item.lvl != curLvl) {
+    //       if (item.lvl > curLvl) {
+    //         curLvl = item.lvl;
+    //         childArr['h' + curLvl].push({ name: item.content, anchor: item.slug });
+    //       } else {
+    //         childArr['h' + (curLvl - 1)][childArr['h' + (curLvl - 1)].length - 1].children = childArr['h' + curLvl];
+    //         childArr['h' + curLvl] = [];
+    //         curLvl--;
+    //         childArr['h' + curLvl].push({ name: item.content, anchor: item.slug });
+    //       }
+    //     } else {
+    //       childArr['h' + curLvl].push({ name: item.content, anchor: item.slug });
+    //     }
+    //   }
+    // }
+
+    // for (var i = 6; i > min; i--) {
+    //   if (childArr['h' + i].length != 0) {
+    //     var index = i - 1;
+    //     childArr['h' + (index)][childArr['h' + (index)].length - 1].children = childArr['h' + i];
+    //   }
+    // }
+
+    // tocResult = childArr['h' + min]; // 将最终计算出来的结果赋值给 tocResult 数组
+
+    // info.toc = tocResult;
+
+
+    function transfer(flatArr) {
+      const stack = [];
+      const result = [];
+
+      var min = 6;
+      for (var i of flatArr) {
+        if (i.lvl < min) {
+          min = i.lvl;
+        }
+      }
+
+      for (const item of flatArr) {
+        const tocItem = createTOCItem(item);
+        if (tocItem.level === min) {
+          result.push(tocItem);
+        }
+        handleItem(tocItem);
+      }
+      return result;
+      function createTOCItem(item) {
+        return {
+          name: item.content,
+          anchor: item.slug,
+          level: item.lvl,
+          children: [],
+        };
+      }
+
+      function handleItem(item) {
+        const top = stack[stack.length - 1];
+        if (!top) {
+          stack.push(item);
+        } else if (item.level > top.level) {
+          // 作为其子目录
+          top.children.push(item);
+          stack.push(item);
+        } else {
+          stack.pop();
+          handleItem(item);
+        }
+      }
+    }
+
+    info.toc = transfer(result);
+
+    delete info.markdownContent;
+
+    // 为各个级别标题添加上 id
+    for(var i of result){
+      switch(i.lvl){
+        case 1:{
+          var newStr = `<h1 id="${i.slug}">`
+          info.htmlContent = info.htmlContent.replace('<h1>',newStr);
+          break;
+        }
+        case 2:{
+          var newStr = `<h2 id="${i.slug}">`
+          info.htmlContent = info.htmlContent.replace('<h2>',newStr);
+          break;
+        }
+        case 3:{
+          var newStr = `<h3 id="${i.slug}">`
+          info.htmlContent = info.htmlContent.replace('<h3>',newStr);
+          break;
+        }
+        case 4:{
+          var newStr = `<h4 id="${i.slug}">`
+          info.htmlContent = info.htmlContent.replace('<h4>',newStr);
+          break;
+        }
+        case 5:{
+          var newStr = `<h5 id="${i.slug}">`
+          info.htmlContent = info.htmlContent.replace('<h5>',newStr);
+          break;
+        }
+        case 6:{
+          var newStr = `<h6 id="${i.slug}">`
+          info.htmlContent = info.htmlContent.replace('<h6>',newStr);
+          break;
+        }
+      }
+    }
+
+    console.log(info,'info:::');
     const blog = await this.ctx.model.Blog.findById(id);
     if (!blog) {
       return null;
